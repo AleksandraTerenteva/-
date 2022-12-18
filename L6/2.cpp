dcaf0e9f0aae649e0126a3bc854eaa838bce91d1
AleksandraTerenteva <49558394+AleksandraTerenteva@users.noreply.github.com>
2022-12-18 20:31:30 +0300

    Create 2.cpp

---
+++ L6/2.cpp
@@ -0,0 +1,8 @@
+#include "2.hpp"
+#include <iostream>
+const unsigned in = monomial2d<1, 2>::value;
+void funct2() {
+    using std::cout;
+    using std::endl;
+    cout << "int2d = " << in << endl;
+}
